1e088faaba237217179e97a0954834cd9ca5437b
Tobias Grelsson <tgrelsson@gmail.com>
2019-04-22 01:12:33 +0200

    cleaned up geometry code

--- geometry.rkt
+++ geometry.rkt
@@ -109,29 +109,28 @@
 
 (define center-tile-vertices
   ((thunk
-    (define x inner)
-    (define corner-vec (let ([z (flsqrt (- 1.0 (* 2.0 x x)))])
-                         (flvector x x z)))
-    (define center-vec
-      (λ ([v : FlVector])
-        (flvector3-sum v (flvector (- x) 0.0 0.0))))
-    (define angle (flvector3-angle (center-vec corner-vec)
+    (let* ([x inner]
+           [corner-vec (let ([z (flsqrt (- 1.0 (* 2.0 x x)))])
+                         (flvector x x z))]
+           [center-vec (λ ([v : FlVector])
+                         (flvector3-sum v (flvector (- x) 0.0 0.0)))]
+           [angle (flvector3-angle (center-vec corner-vec)
                                    (center-vec
                                     (flvector* corner-vec
-                                               (flvector 1.0 -1.0 1.0)))))
-    (define first-edge (build-vector edge-vertex-count
+                                               (flvector 1.0 -1.0 1.0))))]
+           [first-edge (build-vector edge-vertex-count
                                      (λ ([n : Integer])
                                        (quaternion-vector-product
                                         (axis-angle->quaternion (flvector 1.0 0.0 0.0)
                                                                 (angle-fraction angle n edge-vertex-count))
-                                        corner-vec))))
-    (apply vector-append
-           (map (λ ([a : Integer])
-                  (vector-map (curry quaternion-vector-product
-                                     (axis-angle->quaternion (flvector 0.0 0.0 1.0)
-                                                             (angle-fraction tau a 4)))
-                              first-edge))
-                (range 4))))))
+                                        corner-vec)))])
+      (apply vector-append
+             (map (λ ([a : Integer])
+                    (vector-map (curry quaternion-vector-product
+                                       (axis-angle->quaternion (flvector 0.0 0.0 1.0)
+                                                               (angle-fraction tau a 4)))
+                                first-edge))
+                  (range 4)))))))
 
 (define flvector-curry
   (λ ([f : (-> FlVector FlVector FlVector)]
@@ -147,80 +146,80 @@
 
 (define edge-tile-vertices
   ((thunk
-    (define x inner)
-    (define y outer)
-    (define z (flsqrt (- 1.0 (flexpt x 2.0) (flexpt y 2.0))))
-    (define one (let* ([horizontal (flvector 0.0 inner 0.0)]
+    (let* ([x inner]
+           [y outer]
+           [z (flsqrt (- 1.0 (flexpt x 2.0) (flexpt y 2.0)))]
+           [one (let* ([horizontal (flvector 0.0 inner 0.0)]
                        [diagonal diagonal-offset]
                        [rem (flvector3-scale-to (flsqrt (- 1.0
                                                            (flvector3-length-squared horizontal)
                                                            (flvector3-length-squared diagonal)))
                                                 (flvector -1.0 0.0 1.0))])
-                  (flvector3-sum horizontal diagonal rem)))
-    (define two (flvector* one (flvector 1.0 -1.0 1.0)))
-    (define three (flvector y (- x) z))
-    (define four (flvector* three (flvector 1.0 -1.0 1.0)))
-    (define top-angle (vector-angle (flvector-curry flvector3-rejection diagonal-plane)
-                                    one two))
-    (define side-angle (vector-angle (flvector-curry flvector3-subtract (flvector 0.0 inner 0.0))
-                                     one four))
-    (define bottom-angle (vector-angle (flvector-curry flvector3-subtract (flvector outer 0.0 0.0))
-                                       three four))
-    (define build-side
-      (λ ([plane : FlVector]
-          [angle : Flonum]
-          [vector : FlVector])
-        (build-vector edge-vertex-count
-                      (λ ([n : Integer])
-                        (quaternion-vector-product (axis-angle->quaternion
-                                                    plane
-                                                    (angle-fraction angle n edge-vertex-count))
-                                                   vector)))))
-    (vector-append
-     (build-side diagonal-plane top-angle one)
-     (build-side (flvector 0.0 -1.0 0.0) side-angle two)
-     (build-side (flvector -1.0 0.0 0.0) bottom-angle three)
-     (build-side (flvector 0.0 1.0 0.0) side-angle four)))))
+                  (flvector3-sum horizontal diagonal rem))]
+           [two (flvector* one (flvector 1.0 -1.0 1.0))]
+           [three (flvector y (- x) z)]
+           [four (flvector* three (flvector 1.0 -1.0 1.0))]
+           [top-angle (vector-angle (flvector-curry flvector3-rejection diagonal-plane)
+                                    one two)]
+           [side-angle (vector-angle (flvector-curry flvector3-subtract (flvector 0.0 inner 0.0))
+                                     one four)]
+           [bottom-angle (vector-angle (flvector-curry flvector3-subtract (flvector outer 0.0 0.0))
+                                       three four)]
+           [build-side
+            (λ ([plane : FlVector]
+                [angle : Flonum]
+                [vector : FlVector])
+              (build-vector edge-vertex-count
+                            (λ ([n : Integer])
+                              (quaternion-vector-product (axis-angle->quaternion
+                                                          plane
+                                                          (angle-fraction angle n edge-vertex-count))
+                                                         vector))))])
+      (vector-append
+       (build-side diagonal-plane top-angle one)
+       (build-side (flvector 0.0 -1.0 0.0) side-angle two)
+       (build-side (flvector -1.0 0.0 0.0) bottom-angle three)
+       (build-side (flvector 0.0 1.0 0.0) side-angle four))))))
 
 (define corner-tile-vertices
   ((thunk
-    (define x outer)
-    (define z (flsqrt (- 1.0 (* 2.0 x x))))
-    (define one (let* ([diagonal (flvector3-sum
+    (let* ([x outer]
+           [z (flsqrt (- 1.0 (* 2.0 x x)))]
+           [one (let* ([diagonal (flvector3-sum
                                   (flvector3-scale-to plane-gap (flvector 1.0 0.0 1.0))
                                   (flvector3-scale-to plane-gap (flvector 0.0 1.0 1.0)))]
                        [rem (flvector -1.0 -1.0 1.0)])
-                  (flvector3-normal (flvector3-sum diagonal rem))))
-    (define two (let* ([horizontal (flvector 0.0 outer 0.0)]
+                  (flvector3-normal (flvector3-sum diagonal rem)))]
+           [two (let* ([horizontal (flvector 0.0 outer 0.0)]
                        [diagonal diagonal-offset]
                        [rem (flvector3-scale-to (flsqrt (- 1.0
                                                            (flvector3-length-squared horizontal)
                                                            (flvector3-length-squared diagonal)))
                                                 (flvector -1.0 0.0 1.0))])
-                  (flvector3-sum horizontal diagonal rem)))
-    (define three (flvector x x z))
-    (define four (flvector (flvector-ref two 1)
+                  (flvector3-sum horizontal diagonal rem))]
+           [three (flvector x x z)]
+           [four (flvector (flvector-ref two 1)
                            (flvector-ref two 0)
-                           (flvector-ref two 2)))
-    (define top-angle (vector-angle (flvector-curry flvector3-rejection diagonal-plane)
-                                    one two))
-    (define side-angle (vector-angle (flvector-curry flvector3-subtract (flvector 0.0 inner 0.0))
-                                     two three))
-    (define build-side
-      (λ ([plane : FlVector]
-          [angle : Flonum]
-          [vector : FlVector])
-        (build-vector edge-vertex-count
-                      (λ ([n : Integer])
-                        (quaternion-vector-product (axis-angle->quaternion
-                                                    plane
-                                                    (angle-fraction angle n edge-vertex-count))
-                                                   vector)))))
-    (vector-append
-     (build-side diagonal-plane top-angle one)
-     (build-side (flvector 0.0 -1.0 0.0) side-angle two)
-     (build-side (flvector -1.0 0.0 0.0) side-angle three)
-     (build-side (flvector3-normal (flvector 0.0 1.0 1.0)) top-angle four)))))
+                           (flvector-ref two 2))]
+           [top-angle (vector-angle (flvector-curry flvector3-rejection diagonal-plane)
+                                    one two)]
+           [side-angle (vector-angle (flvector-curry flvector3-subtract (flvector 0.0 inner 0.0))
+                                     two three)]
+           [build-side
+            (λ ([plane : FlVector]
+                [angle : Flonum]
+                [vector : FlVector])
+              (build-vector edge-vertex-count
+                            (λ ([n : Integer])
+                              (quaternion-vector-product (axis-angle->quaternion
+                                                          plane
+                                                          (angle-fraction angle n edge-vertex-count))
+                                                         vector))))])
+      (vector-append
+       (build-side diagonal-plane top-angle one)
+       (build-side (flvector 0.0 -1.0 0.0) side-angle two)
+       (build-side (flvector -1.0 0.0 0.0) side-angle three)
+       (build-side (flvector3-normal (flvector 0.0 1.0 1.0)) top-angle four))))))
 
 (define face-orientations
   (list
@@ -233,54 +232,69 @@
            0 -1 0
            0 0 -1)))
 
+(define face-tile-vertices
+  (λ ([x : Integer]
+      [y : Integer])
+    (let* ([rotation (λ ([v : (Vectorof FlVector)])
+                       (λ ([a : Flonum])
+                         (vector-map (curry quaternion-vector-product
+                                            (axis-angle->quaternion (flvector 0.0 0.0 1.0) (* a tau)))
+                                     v)))]
+           [rotated-edge (rotation edge-tile-vertices)]
+           [rotated-corner (rotation corner-tile-vertices)])
+      (match (list x y)
+        ['(0 0) center-tile-vertices]
+        ['(-1 0) edge-tile-vertices]
+        ['(0 1) (rotated-edge 0.25)]
+        ['(1 0) (rotated-edge 0.5)]
+        ['(0 -1) (rotated-edge 0.75)]
+        ['(-1 -1) corner-tile-vertices]
+        ['(-1 1) (rotated-corner 0.25)]
+        ['(1 1) (rotated-corner 0.5)]
+        ['(1 -1) (rotated-corner 0.75)]
+        [default (begin
+                   (error "invalid position")
+                   (vector))]))))
+
+(define face-tile
+  (let ([no-color (flcolor 0.0 0.0 0.0 0.0)]
+        [no-rotation (flvector 0.0 0.0 0.0)])
+    (λ ([n : Integer])
+      (let* ([x (- (modulo n 3) 1)]
+             [y (- (floor (/ n 3)) 1)]
+             [center (flvector (fl x) (fl y) 1.5)])
+        (tile
+         no-color
+         (vector x y 1)
+         (vector 0 0 1)
+         no-rotation
+         (flvector3-normal center)
+         (face-tile-vertices x y))))))
+
 (define face
-  (build-vector 9 (λ ([n : Integer])
-                    (let* ([x (- (modulo n 3) 1)]
-                           [y (- (floor (/ n 3)) 1)]
-                           [center (flvector (fl x) (fl y) 1.5)]
-                           [rotation (λ ([v : (Vectorof FlVector)])
-                                       (λ ([a : Flonum])
-                                         (vector-map (curry quaternion-vector-product
-                                                            (axis-angle->quaternion (flvector 0.0 0.0 1.0) (* a tau)))
-                                                     v)))]
-                           [rotated-edge (rotation edge-tile-vertices)]
-                           [rotated-corner (rotation corner-tile-vertices)])
-                      (tile
-                       (flcolor 0.0 0.0 0.0 0.0)
-                       (vector x y 1)
-                       (vector 0 0 1)
-                       (flvector 0.0 0.0 0.0)
-                       (flvector3-normal center)
-                       (cond
-                         [(= 0 x y) center-tile-vertices]
-                         [(and (= -1 x) (= 0 y)) edge-tile-vertices]
-                         [(and (= 0 x) (= 1 y)) (rotated-edge 0.25)]
-                         [(and (= 1 x) (= 0 y)) (rotated-edge 0.5)]
-                         [(and (= 0 x) (= -1 y)) (rotated-edge 0.75)]
-                         [(= -1 x y) corner-tile-vertices]
-                         [(and (= -1 x) (= 1 y)) (rotated-corner 0.25)]
-                         [(and (= 1 x) (= 1 y)) (rotated-corner 0.5)]
-                         [(and (= 1 x) (= -1 y)) (rotated-corner 0.75)]
-                         [else (vector)]))))))
+  (build-vector 9 face-tile))
+
+(define tiles
+  (λ ([colors : (Listof flcolor)]
+      [orientations : (Listof (Vectorof Integer))])
+    (apply vector-append
+           (map (λ ([color : flcolor]
+                    [orientation : (Vectorof Integer)])
+                  (vector-map (λ ([t : tile])
+                                (tile
+                                 color
+                                 (matrix-vector-product
+                                  orientation
+                                  (tile-position t))
+                                 (matrix-vector-product
+                                  orientation
+                                  (tile-normal t))
+                                 (build-flvector 9 (compose fl (ref orientation)))
+                                 (tile-center-vertex t)
+                                 (tile-edge-vertices t)))
+                              face))
+                colors
+                orientations))))
 
 (define (make-tiles)
-  (apply vector-append
-         (map (λ ([n : Integer]
-                  [color : flcolor]
-                  [orientation : (Vectorof Integer)])
-                (vector-map (λ ([t : tile])
-                              (tile
-                               color
-                               (matrix-vector-product
-                                orientation
-                                (tile-position t))
-                               (matrix-vector-product
-                                orientation
-                                (tile-normal t))
-                               (build-flvector 9 (compose fl (ref orientation)))
-                               (tile-center-vertex t)
-                               (tile-edge-vertices t)))
-                            face))
-              (range 6)
-              rubik-colors
-              face-orientations)))
+  (tiles rubik-colors face-orientations))
